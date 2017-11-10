f3cbcbbe637109fec233b78ae2c05eb35f311231
Raj Singh <rrsingh@us.ibm.com>
2017-11-09 23:44:36 -0500

    format nicer

--- src/components/progress.js
+++ src/components/progress.js
@@ -23,13 +23,13 @@ class Progress extends Component {
       if (this.props.olliPosition.nextStop) {
         stop2String += `NEXT: ${this.props.olliPosition.nextStop.name}`;
       }
-      let coords = `${this.props.olliPosition.coordinates[0]}, ${this.props.olliPosition.coordinates[1]}`;
+      let coords = `${(this.props.olliPosition.coordinates[0]+" ").slice(0,6)}, ${(this.props.olliPosition.coordinates[1]+" ").slice(0,6)}`;
       return (
-        <div style={{margin: 10}} className="absolute top right">
-          <h3>{coords}</h3>
+        <div style={{margin: 10}} className="absolute bottom right">
+          <h3>Coords: {coords}</h3>
           <h3>{stop1String}</h3>
           <h3>{stop2String}</h3>
-          <h3>{this.props.olliPosition.nextStopProgress}</h3>
+          <h3>progress: {Math.round(this.props.olliPosition.nextStopProgress * 100)}%</h3>
         </div>
       );
     }
