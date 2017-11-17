d5af3210d830e2bb7c090836a0ab3765bd7b9381
Raj Singh <rrsingh@us.ibm.com>
2017-11-17 11:29:17 -0500

    tells the real time now

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 export default class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval( () => 
+            this.tick(), 1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
     render() {
         return (
             <div className="stop-panel">
                 <h2>Time</h2>
-                <div className="clock">7:54 AM</div>
+                <div className="clock">{this.state.date.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'})}</div>
             </div>
         );
     }
