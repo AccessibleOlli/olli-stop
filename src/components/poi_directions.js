18fd17ad4d1d30eaa429a77b3ebe393c7d804343
Raj Singh <rrsingh@us.ibm.com>
2018-01-09 10:54:38 -0800

    better directions notification

--- src/components/poi_directions.js
+++ src/components/poi_directions.js
@@ -4,9 +4,16 @@ import { bindActionCreators } from 'redux';
 import sendDirectionsSMS from '../util/sms_directions';
 
 class POIDirections extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      directionsSent: false
+    }
+  }
 
   text() {
     sendDirectionsSMS(this.props.poiDirections, this.props.activePersona);
+    this.setState({directionsSent:true});
   }
 
   render() {
@@ -42,20 +49,28 @@ class POIDirections extends Component {
         );
       }))
     }
+    let sentMessage = <td>
+      <button className="text-directions-button" onClick={(e) => this.text()}>
+        <img src="./img/iphone.png" />Send to Phone
+      </button>
+    </td>;
+
+    if (this.state.directionsSent) {
+      sentMessage = <td><h2>Directions sent to your phone!</h2></td>
+      // this.setState({directionsSent:false});
+    }
+
     return (
       <div className="directions">
         <table style={{width: '100%'}}>
+          <tbody>
           <tr>
             <td style={{textAlign: 'right'}}>directions provided by <img className="mapbox-logo" src="./img/mapbox-logo-color.svg" /> </td>  
           </tr>
           <tr>
-            <td>
-              <button className="text-directions-button" onClick={(e) => this.text()}>
-                <img src="./img/iphone.png" />
-                Send to Phone
-              </button>
-            </td>
+            {sentMessage}
           </tr>
+          </tbody>
         </table>
         <div className="directions-control directions-control-directions">
           <div className="mapbox-directions-instructions">
